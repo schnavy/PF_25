093359f5e922ec58cea2b68eb70d35716c426788
David Wahrenburg <mail@davidwahrenburg.de>
2025-11-25 13:50:04 +0100

    add github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import rehypeTitleFigure from 'rehype-title-figure';
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    rehypePlugins: [rehypeTitleFigure]
-  }
+    markdown: {
+        // @ts-expect-error - rehype-title-figure uses older unified types
+        rehypePlugins: [rehypeTitleFigure]
+    },
+    site: 'http://schnavy.github.io/',
+    base: '/PF_25',
 });
